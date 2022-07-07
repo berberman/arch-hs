009a8877e4529dc517f51164f1939b68f0e344a2
Potato Hatsue <1793913507@qq.com>
2022-07-07 01:08:59 -0400

    Support Cabal 3.6

--- src/Distribution/ArchHs/Compat.hs
+++ src/Distribution/ArchHs/Compat.hs
@@ -4,11 +4,17 @@
 module Distribution.ArchHs.Compat
   ( pattern PkgFlag,
     PkgFlag,
+    licenseFile,
   )
 where
 
+import Data.Maybe (listToMaybe)
 import Distribution.Types.ConfVar
 import Distribution.Types.Flag
+import Distribution.Types.PackageDescription (PackageDescription, licenseFiles)
+#if MIN_VERSION_Cabal(3,6,0)
+import Distribution.Utils.Path (getSymbolicPath)
+#endif
 
 pattern PkgFlag :: FlagName -> ConfVar
 {-# COMPLETE PkgFlag #-}
@@ -19,4 +25,11 @@ pattern PkgFlag x = PackageFlag x
 #else
 type PkgFlag = Flag
 pattern PkgFlag x = Flag x
+#endif
+
+licenseFile :: PackageDescription -> Maybe FilePath
+#if MIN_VERSION_Cabal(3,6,0)
+licenseFile = fmap getSymbolicPath . listToMaybe . licenseFiles
+#else
+licenseFile = listToMaybe . licenseFiles
 #endif
