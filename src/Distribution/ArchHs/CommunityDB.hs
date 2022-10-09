db537bfdc56058ded7bfb57ee7d7bbff5bb849ae
Potato Hatsue <1793913507@qq.com>
2022-10-08 22:21:40 -0400

    Update alpm

--- src/Distribution/ArchHs/CommunityDB.hs
+++ src/Distribution/ArchHs/CommunityDB.hs
@@ -36,34 +36,74 @@ import Data.Maybe (mapMaybe)
 
 #ifdef ALPM
 {-# LANGUAGE ForeignFunctionInterface #-}
-import qualified Data.Sequence as Seq
-import Data.Foldable (toList)
+
 import Data.IORef (IORef, modifyIORef', newIORef, readIORef)
+import Distribution.ArchHs.Utils (extractFromEVR)
 import Foreign.C.String (CString, peekCString)
 import Foreign.Ptr (FunPtr, freeHaskellFunPtr)
 
 foreign import ccall "wrapper"
-  wrap :: (CString -> CString -> IO ()) -> IO (FunPtr (CString -> CString -> IO ()))
+  wrap :: (CString -> CString -> CString -> CString -> IO ()) -> IO (FunPtr (CString -> CString -> CString -> CString -> IO ()))
 
 foreign import ccall "clib.h query_community"
-  query_community :: FunPtr (CString -> CString -> IO ()) -> IO ()
+  query_community :: FunPtr (CString -> CString -> CString -> CString -> IO ()) -> FunPtr (CString -> CString -> CString -> CString -> IO ()) -> IO ()
 
-callback :: IORef (Seq.Seq (ArchLinuxName, ArchLinuxVersion)) -> CString -> CString -> IO ()
-callback ref x y = do
-  x' <- peekCString x
-  y' <- peekCString y
-  modifyIORef' ref (Seq.|> (ArchLinuxName x', extractFromEVR y'))
+type RawPkgSet = IORef (Map.Map ArchLinuxName ((ArchLinuxVersion, String, String), IORef (Map.Map String [(ArchLinuxName, Maybe ArchLinuxVersion)])))
+
+pkgCallback :: RawPkgSet -> CString -> CString -> CString -> CString -> IO ()
+pkgCallback ref name version desc url = do
+  name' <- peekCString name
+  version' <- peekCString version
+  desc' <- peekCString desc
+  url' <- peekCString url
+  m <- newIORef Map.empty
+  modifyIORef' ref (Map.insert (ArchLinuxName name') ((extractFromEVR version', desc', url'), m))
+
+listCallback :: RawPkgSet -> CString -> CString -> CString -> CString -> IO ()
+listCallback ref name key dm dv = do
+  name' <- peekCString name
+  key' <- peekCString key
+  dm' <- peekCString dm
+  dv' <- peekCString dv
+  s <- snd . (\r -> r Map.! ArchLinuxName name') <$> readIORef ref
+  modifyIORef' s (Map.insertWith (++) key' [(ArchLinuxName dm', if null dv' then Nothing else Just (extractFromEVR dv'))])
 
 -- | The same purpose as 'loadCommunity' but use alpm to query community db instead.
 loadCommunityDBFFI :: IO CommunityDB
 loadCommunityDBFFI = do
-  ref <- newIORef Seq.empty
-  callbackW <- wrap $ callback ref
-  query_community callbackW
-  freeHaskellFunPtr callbackW
-  Map.fromList . toList <$> readIORef ref
+  ref <- newIORef Map.empty
+  pkgCallbackW <- wrap $ pkgCallback ref
+  listCallbackW <- wrap $ listCallback ref
+  query_community pkgCallbackW listCallbackW
+  freeHaskellFunPtr pkgCallbackW
+  freeHaskellFunPtr listCallbackW
+  s <- readIORef ref
+  re <-
+    mapM
+      ( \(name, ((ver, desc, url), r)) -> do
+          l <- readIORef r
+          let f (Just xs) = uncurry PkgDependent <$> xs
+              f Nothing = []
+          pure
+            ( name,
+              PkgDesc
+                { _name = name,
+                  _version = ver,
+                  _desc = desc,
+                  _url = if null url then Nothing else Just url,
+                  _depends = f $ l Map.!? "depends",
+                  _provides = f $ l Map.!? "provides",
+                  _conflicts = f $ l Map.!? "conflicts",
+                  _optDepends = f $ l Map.!? "optdepends",
+                  _makeDepends = f $ l Map.!? "makedepends",
+                  _checkDepends = f $ l Map.!? "checkdepends",
+                  _replaces = f $ l Map.!? "replaces"
+                }
+            )
+      )
+      $ Map.toList s
+  pure $ Map.fromList re
 #endif
-
 -----------------------------------------------------------------------------
 
 -- | Default path to @community.db@.
