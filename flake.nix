6126ef9f68e3af16297b7d78ffe8dd1ea66f3a9d
Potato Hatsue <1793913507@qq.com>
2023-06-08 13:28:22 +0800

    Update nix

--- flake.nix
+++ flake.nix
@@ -5,25 +5,30 @@
     let
       pkgs = import nixpkgs {
         system = "x86_64-linux";
-        overlays = [ self.overlay ];
-        config = { allowBroken = true; };
+        overlays = [ self.overlays.default ];
       };
     in with pkgs; {
-      overlay = self: super:
+      overlays.default = final: prev:
         let
-          hpkgs = super.haskellPackages.override{
-            overrides = hself: hsuper:{
-              arch-web = with super.haskell.lib; disableLibraryProfiling (dontCheck hsuper.arch-web);
+          hpkgs = prev.haskellPackages.override {
+            overrides = hself: hsuper: {
+              arch-web = prev.haskell.lib.dontCheck
+                (hself.callCabal2nix "arch-web" (builtins.fetchTarball {
+                  url =
+                    "https://hackage.haskell.org/package/arch-web-0.2/arch-web-0.2.tar.gz";
+                  sha256 =
+                    "0306ky9a4rfc4lz838kykl91br6s7sm9dkp446425g3yjh6x26zl";
+                }) { });
             };
           };
           arch-hs = hpkgs.callCabal2nix "arch-hs" ./. { };
-        in with super;
+        in with final;
         with haskell.lib; {
           inherit arch-hs;
           arch-hs-dev =
             addBuildTools arch-hs [ cabal-install haskell-language-server ];
         };
-      defaultPackage.x86_64-linux = arch-hs;
-      devShell.x86_64-linux = arch-hs-dev.envFunc { };
+      packages.x86_64-linux.default = arch-hs;
+      devShells.x86_64-linux.default = arch-hs-dev.envFunc { };
     };
 }
